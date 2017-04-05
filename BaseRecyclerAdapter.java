7a7b713f760838b40c41cec1332b069b69a15f99
a_zhon <958460248@qq.com>
2017-04-05 14:14:30 +0800

    增加了添加头尾布局的方法

--- BaseRecyclerAdapter.java
+++ BaseRecyclerAdapter.java
@@ -1,6 +1,7 @@
-package com.hjtech.rabbit.express.function;
+package com.azhong.adapter;
 
 import android.support.annotation.DrawableRes;
+import android.support.annotation.LayoutRes;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.util.SparseArray;
@@ -29,7 +30,26 @@ public abstract class BaseRecyclerAdapter<D, VH extends ViewHolder> extends Recy
      * 布局资源id
      */
     private int layoutResId;
-
+    /**
+     * 头部布局id
+     */
+    private int headView = -1;
+    /**
+     * 底部部布局id
+     */
+    private int footView = -1;
+    /**
+     * 数据显示itemType
+     */
+    private final int ITEM_TYPE = 870371;
+    /**
+     * 头部itemType
+     */
+    private final int HEAD_TYPE = 870372;
+    /**
+     * 底部itemType
+     */
+    private final int FOOT_TYPE = 870373;
     /**
      * Item点击事件
      */
@@ -39,7 +59,7 @@ public abstract class BaseRecyclerAdapter<D, VH extends ViewHolder> extends Recy
      */
     private onItemLongClickListener longListener;
 
-    public BaseRecyclerAdapter(int layoutResId, List<D> data) {
+    public BaseRecyclerAdapter(@LayoutRes int layoutResId, List<D> data) {
         this.data = data == null ? new ArrayList<D>() : data;
         if (layoutResId != 0) {
             this.layoutResId = layoutResId;
@@ -50,22 +70,69 @@ public abstract class BaseRecyclerAdapter<D, VH extends ViewHolder> extends Recy
 
     @Override
     public VH onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(layoutResId, parent, false);
+        View view = null;
+        switch (viewType) {
+            case ITEM_TYPE:
+                view = LayoutInflater.from(parent.getContext()).inflate(layoutResId, parent, false);
+                break;
+            case HEAD_TYPE:
+                view = LayoutInflater.from(parent.getContext()).inflate(headView, parent, false);
+                break;
+            case FOOT_TYPE:
+                view = LayoutInflater.from(parent.getContext()).inflate(footView, parent, false);
+                break;
+        }
         return (VH) new BaseViewHolder(view);
     }
 
     @Override
+    public int getItemViewType(int position) {
+        int type = ITEM_TYPE;
+        //如果添加了头部view
+        if (position == 0 && headView != -1) {
+            type = HEAD_TYPE;
+        }
+        //如果添加了底部view
+        if (position == getItemCount() - 1 && footView != -1) {
+            type = FOOT_TYPE;
+        }
+        return type;
+    }
+
+    @Override
     public void onBindViewHolder(VH holder, int position) {
-        //设置Item的点击事件
-        holder.itemView.setOnClickListener(this);
-        //设置Item的长按事件
-        holder.itemView.setOnLongClickListener(this);
-        holder.itemView.setTag(position);
-        bindTheData(holder, data.get(position), position);
+        switch (holder.getItemViewType()) {
+            case ITEM_TYPE:
+                //设置Item的点击事件
+                holder.itemView.setOnClickListener(this);
+                //设置Item的长按事件
+                holder.itemView.setOnLongClickListener(this);
+                if (headView == -1) {
+                    holder.itemView.setTag(position);
+                    bindTheData(holder, data.get(position), position);
+                } else {
+                    holder.itemView.setTag(position - 1);
+                    bindTheData(holder, data.get(position - 1), position - 1);
+                }
+                break;
+            case HEAD_TYPE:
+                break;
+            case FOOT_TYPE:
+                break;
+        }
     }
 
     @Override
     public int getItemCount() {
+        if (headView != -1 && footView != -1) {
+            return data.size() + 2;
+        }
+        if (headView != -1) {
+            return data.size() + 1;
+        }
+        if (footView != -1) {
+            return data.size() + 1;
+        }
         return data.size();
     }
 
@@ -86,6 +153,20 @@ public abstract class BaseRecyclerAdapter<D, VH extends ViewHolder> extends Recy
      * @param data   数据源
      */
     protected abstract void bindTheData(VH holder, D data, int position);
+
+    /**
+     * 添加头部View
+     */
+    public void addHeadView(@LayoutRes int id) {
+        this.headView = id;
+    }
+
+    /**
+     * 添加底部View
+     */
+    public void addFootView(@LayoutRes int id) {
+        this.footView = id;
+    }
 
     @Override
     public void onClick(View v) {
@@ -156,7 +237,7 @@ public abstract class BaseRecyclerAdapter<D, VH extends ViewHolder> extends Recy
      *
      * @param clickListener 监听器
      */
-    public void setClickListener(onItemClickListener clickListener) {
+    public void setItemClickListener(onItemClickListener clickListener) {
         this.clickListener = clickListener;
     }
 
@@ -165,15 +246,15 @@ public abstract class BaseRecyclerAdapter<D, VH extends ViewHolder> extends Recy
      *
      * @param longListener 监听器
      */
-    public void setLongListener(onItemLongClickListener longListener) {
+    public void setItemLongClickListener(onItemLongClickListener longListener) {
         this.longListener = longListener;
     }
 
-    interface onItemClickListener {
+    public interface onItemClickListener {
         void onItemClick(int position, View v);
     }
 
-    interface onItemLongClickListener {
+    public interface onItemLongClickListener {
         boolean onItemLonClick(int position, View v);
     }
 }
