98d020ae5abce7d04d6a8101d089c6396d9bd5e1
钟仕垣 <958460248@qq.com>
2017-05-24 17:47:58 +0800

    添加获取屏幕大小、sp存储Utils

---
+++ ScreenSizeUtils.java
@@ -0,0 +1,69 @@
+﻿package cn.hjtech.pigeon.common.utils;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+/**
+ * 文件名:    SharePreUtils
+ * 创建时间:  2017/05/24 on 17:58
+ * 描述: 
+ * 	1.获取手机屏幕的宽高
+ * 	2.dip 与 px 之间的转换
+ *
+ * @see android.support.annotation.Px
+ */
+
+public class ScreenSizeUtils {
+
+    private static ScreenSizeUtils instance = null;
+    private int screenWidth, screenHeight;
+
+    public static ScreenSizeUtils getInstance(Context mContext) {
+        if (instance == null) {
+            synchronized (ScreenSizeUtils.class) {
+                if (instance == null)
+                    instance = new ScreenSizeUtils(mContext);
+            }
+        }
+        return instance;
+    }
+
+    private ScreenSizeUtils(Context mContext) {
+        WindowManager manager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        manager.getDefaultDisplay().getMetrics(dm);
+        // 获取屏幕分辨率宽度
+        screenWidth = dm.widthPixels;
+        // 获取屏幕分辨率高度
+        screenHeight = dm.heightPixels;
+    }
+
+    //获取屏幕宽度
+    public int getScreenWidth() {
+        return screenWidth;
+    }
+
+    //获取屏幕高度
+    public int getScreenHeight() {
+        return screenHeight;
+    }
+
+    /**
+     * 根据手机的分辨率从 dip 的单位 转成为 px(像素)
+     *
+     * @return px
+     */
+    private int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
